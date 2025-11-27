da8f26ed5e4fef240843fc24ee5689b668731e3e
emiramus <em31129@seeu.edu.mk>
2025-11-28 00:49:24 +0100

    week6

--- week6/UV Mapping/main.js
+++ week6/UV Mapping/main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 3;
@@ -38,18 +37,14 @@ const uvs = [
     0.25, 0.66, 0, 0.66,
 ];
 
-// Apply custom UV mapping
 geometry.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
 
-// Create a material with the texture map
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-
-// Render loop to display the scene
 function animate() {
     requestAnimationFrame(animate);
    
